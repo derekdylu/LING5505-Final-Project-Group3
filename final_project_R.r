823f077ed629e2f7a4d5ee3b576f00536f8d11f4
TimmyChu890902 <71302486+TimmyChu890902@users.noreply.github.com>
2021-06-08 23:28:56 +0800

    Update final_project_R.r

--- final_project_R.r
+++ final_project_R.r
@@ -10,14 +10,14 @@ getwd()
 
 
 
-text <- list.files(path = "./", full.names = FALSE)## ÀÉ®×
-paragraph_num <- vector("numeric", len?th = length(text))## number of paragraph
+text <- list.files(path = "./", full.names = FALSE)## AER¡Ñ
+paragraph_num <- vector("numeric", le?gth = length(text))## number of paragraph
 article_len <- vector("numeric", length = length(text))## length of article
 sentence_num <- vector("numeric", length = length(text))## number of sentencce
 
 post <- list()
 for(i in seq_along(text)){
-  post[[i]] <- re?dLines(text[i], encoding = "UTF-8", warn = FALSE)
+  post[[i]] <- r?adLines(text[i], encoding = "UTF-8", warn = FALSE)
   paragraph_num[i] <- length(post[[i]])
   a <- post[[i]][1]
   for(j in 2 : length(post[[i]])){
@@ -28,8 +28,8 @@ for(i in seq_along(text)){
 }
 
 post <- unlist(post)## turn to vector
-for(i in seq_?long(post)){ 
-  sentence_num[i] <- length(unlist(strsplit(post[i], split = "[¡C¡H¡I¡K]", fixed = FALSE)))
+for(i in seq?along(post)){ 
+  sentence_num[i] <- length(unlist(strsplit(post[i], split = "[¡C¡I¡H]", fixed = FALSE)))
 }
 
 
@@ -37,7 +37,7 @@ seg <- worker()
 content <- vector("character", length(post))
 for(i in seq_along(post)){
   segged <- segment(post[i], seg)
-  article_len[i] <- ncha?(paste(segged, collapse = ""))
+  article_len[i] <- nchar?paste(segged, collapse = ""))
   content[i] <- paste(segged, collapse = "\u3000")
 }
 
@@ -50,17 +50,9 @@ df_unbreak <- tibble::tibble(
   id = text,
   par_num = paragraph_num,
   sentence_num = sentence_num,
-  arti?le_len = article_len,
+  artic?e_len = article_len,
   content = post
 )
 
 df_break
 df_unbreak
-# import("CwnGraph", "DistilTag", "CwnSenseTagger")
-# CwnGraph.download()
-# DistilTag.download()
-# CwnSenseTagger.download()
-# from CwnGraph import CwnBase
-# cwn = CwnBase()
-# cwn.find_lemma("^???¨¥$")[0].senses
-
